ff4b1509ea85e0daffdf4c5f9f64ea2a18d627b9
Jienshakh <jeydin@mail.ru>
2021-09-05 02:55:35 +0900

    exercise to lesson 2.12

---
+++ 2.12/script/2.12.js
@@ -0,0 +1,18 @@
+'use strict'
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+let personalMovieDB  = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+
+personalMovieDB.movies[prompt('Один из последних просмотренных фильмов', '')] = 
+prompt('На сколько оцените его', '');
+personalMovieDB.movies[prompt('Один из последних просмотренных фильмов', '')] = 
+prompt('На сколько оцените его', '');
+
+
